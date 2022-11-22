a3f08c5ac0f84b1576f99ac4a7d49792e2344bee
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2022-11-22 11:33:34 -0500

    rewrite code in index.js to make it responsive

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 import { getResistorOhms } from './resistor.js';
 
+const calc = document.querySelector('.calculator');
+const ceramic = document.querySelector('.ceramic');
+const answer = document.querySelector('.answer');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-console.log(answer);
+calc.addEventListener('click', (evt) => {
+  if (!evt.composedPath()[1].id.includes('color')) {
+    return;
+  }
+  const colorPosition = evt.composedPath()[1].id[5];
+  const cl = ceramic.children[colorPosition];
+  cl.classList.replace(cl.classList[1], evt.composedPath()[0].innerHTML);
+  const colors = [
+    ceramic.children[0].classList[1],
+    ceramic.children[1].classList[1],
+    ceramic.children[2].classList[1],
+    ceramic.children[3].classList[1],
+  ];
+  answer.innerText = getResistorOhms(colors);
+});
